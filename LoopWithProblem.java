d35b0d8666ea36c46d2c5f37e7f7f849bebad87b
SaitovN <nuriksaitov03@gmailcom>
2023-06-02 16:47:19 +0600

    Create LoopWithProblem.java
    LoopWithProblem

---
+++ LoopWithProblem.java
@@ -0,0 +1,9 @@
+package academy.devonline.java.basic.section01_setup.section05_setup;
+
+public class LoopWithProblem {
+    public static void main(String[] args) {
+        for (int i =0 ; i<=5 ; i++){
+            /*i--;*/
+        }
+    }
+}
